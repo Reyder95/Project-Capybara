1b2abadaec5c8225625886dc9b60ca1fec0f49c5
Reyder95 <khoutas98@gmail.com>
2026-09-04 14:06:11 -0400

    Upgraded to 6000.0.83f1

--- Assets/Code/Scripts/BetterJump.cs
+++ Assets/Code/Scripts/BetterJump.cs
@@ -18,13 +18,13 @@ public class BetterJump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (rb.velocity.y < 0)
+        if (rb.linearVelocity.y < 0)
         {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
-        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        else if (rb.linearVelocity.y > 0 && !Input.GetButton("Jump"))
         {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
 }
--- Assets/Code/Scripts/EnemyAI.cs
+++ Assets/Code/Scripts/EnemyAI.cs
@@ -183,7 +183,7 @@ public class EnemyAI : MonoBehaviour
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         if (rb != null)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
     }
 
